c3650a4b94bed41774e4a45199acf8cb6c69c19a
Louis-Saglio <louis.saglio@sfr.fr>
2017-12-16 23:38:26 +0100

    adapt to new input_controller_version

--- src/controller.py
+++ src/controller.py
@@ -1,15 +1,15 @@
+import settings
+from src.file_manager import FileManager
 from src.image_downloader import ImageDownloader
 from src.input_controller import InputController
 from src.theme_manager import ThemeManager
-from src.file_manager import FileManager
-import settings
 
 
 class Controller:
 
     def __init__(self):
         self.settings = settings
-        self.input_controller = InputController()
-        self.theme_manager = ThemeManager(self.input_controller.get_theme_name())
+        self.input_controller = InputController(self)
+        self.theme_manager = ThemeManager(self.input_controller.get_theme_name(auto=True))
         self.file_manager = FileManager(self)
         self.image_downloader = ImageDownloader(self)
