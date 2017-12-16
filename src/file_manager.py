d4cd38ab704b3506a1d69b62fdb46e0b902df5d5
Louis-Saglio <louis.saglio@sfr.fr>
2017-12-16 23:40:00 +0100

    get_random_file_name() save_to_download() remove existing data dir if conflict

--- src/file_manager.py
+++ src/file_manager.py
@@ -1,5 +1,7 @@
 import os
-import settings
+import shutil
+
+from src.utils import get_random_string
 
 
 class FileManager:
@@ -11,11 +13,28 @@ class FileManager:
         self.controller = controller
         self.root_dir = self.get_root_dir()
         self.telechargement_dir = os.path.join(self.root_dir, self.controller.settings.TELECHARGEMENT_DIR)
-        os.makedirs(self.root_dir)
+        if os.path.isdir(self.root_dir):
+            shutil.rmtree(self.root_dir)
         os.makedirs(self.telechargement_dir)
 
     def get_root_dir(self):
-        return os.path.join(settings.APP_DIR, self.controller.theme_manager.name)
+        """Retourne le dossier racine des données de l'application"""
+        return os.path.join(self.controller.settings.APP_DIR, self.controller.theme_manager.name)
 
     def get_saved_urls_file_path(self):
         return os.path.join(self.root_dir, self.controller.settings.SAVED_URLS_FILE)
+
+    def get_random_file_name(self, directory):
+        filename = None
+        while filename in os.listdir(directory) or filename is None:
+            filename = get_random_string(
+                self.controller.settings.CHAR_FOR_FILE_NAME,
+                self.controller.settings.FILE_NAME_LENGTH
+            )
+        return filename
+
+    def save_to_download(self, content, file_name=None):
+        if file_name is None:
+            file_name = self.get_random_file_name(self.telechargement_dir)
+        with open(os.path.join(self.telechargement_dir, file_name), 'wb') as f:
+            f.write(content)
