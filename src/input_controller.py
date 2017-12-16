ec4b3cfc726e4c3ebb6608f8bd49d753ba4e142b
Louis-Saglio <louis.saglio@sfr.fr>
2017-12-16 23:41:03 +0100

    real get_theme_name()

--- src/input_controller.py
+++ src/input_controller.py
@@ -1,4 +1,9 @@
 class InputController:
 
-    def get_theme_name(self):
-        return 'abc'
+    def __init__(self, controller):
+        self.controller = controller
+
+    def get_theme_name(self, auto=True):
+        if auto:
+            return self.controller.settings.DEFAULT_APP_NAME
+        return input("Choisissez un nom pour ce système de getsion : >>> ")
