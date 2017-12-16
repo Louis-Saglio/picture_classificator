4264c6ae28a9e59ad9baaa492aacf99ff4a25870
Louis-Saglio <louis.saglio@sfr.fr>
2017-12-16 23:43:36 +0100

    CHAR_FOR_FILE_NAME, FILE_NAME_LENGTH, DEFAULT_APP_NAME

--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
 APP_DIR = '/home/louis/Projects/Python/picture_classificator/app_data'
 TELECHARGEMENT_DIR = 'downloads'
 SAVED_URLS_FILE = "saved_urls"
+CHAR_FOR_FILE_NAME = "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN1234567890_-"
+FILE_NAME_LENGTH = 9
+DEFAULT_APP_NAME = "default"
