5efc7268c6df38e01ca1740e16d2900420e11a6d
Louis-Saglio <louis.saglio@sfr.fr>
2017-12-17 01:54:14 +0100

    Clean old code

--- main/main.py
+++ main/main.py
@@ -1,8 +1,3 @@
-from src.theme_manager import ThemeManager
+from src.controller import Controller
 
-theme = ThemeManager()
-theme.add_keyword("Arbre", 0)
-theme.add_ranked_keywords(1, "Branche", "Tronc")
-theme.add_keyword("Feuille", 2)
-theme.get_images(30)
-theme.process()
+controller = Controller()
