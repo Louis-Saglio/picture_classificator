a336dacf72d042c276b00ccdd2b58f181438aaae
Louis-Saglio <louis.saglio@sfr.fr>
2017-12-17 16:57:57 +0100

    Tests

--- tests/test_imageComparator.py
+++ tests/test_imageComparator.py
@@ -1,7 +1,6 @@
+import os
 import shutil
 from unittest import TestCase
-
-import os
 
 from src.controller import Controller
 
@@ -10,10 +9,18 @@ class TestImageComparator(TestCase):
 
     def setUp(self):
         self.controller = Controller()
-        self.controller.image_downloader.download_images()
 
     def test_show_image(self):
+        self.controller.image_downloader.download_images()
         self.controller.image_comparator.show_image(os.listdir(self.controller.file_manager.telechargement_dir)[0])
+
+    def test_build_app(self):
+        self.controller.image_comparator.build_app()
+
+    def test_compare(self):
+        self.controller.image_downloader.download_images()
+        self.controller.image_comparator.build_app()
+        self.controller.image_comparator.compare()
 
     def __del__(self):
         shutil.rmtree(self.controller.file_manager.root_dir)
