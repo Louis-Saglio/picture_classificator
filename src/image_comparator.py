b529d488b7012162ac15a17ff506f3a80c86ec28
Louis-Saglio <louis.saglio@sfr.fr>
2017-12-17 16:54:38 +0100

    Improve show_image() API New build_app() New Compare()

--- src/image_comparator.py
+++ src/image_comparator.py
@@ -1,4 +1,6 @@
 import os
+import random
+
 from PIL import Image
 
 
@@ -9,7 +11,22 @@ class ImageComparator:
         :type controller: src.controller.Controller
         """
         self.controller = controller
+        self.images = {}
 
-    def show_image(self, name):
-        with Image.open(os.path.join(self.controller.file_manager.telechargement_dir, name)) as img:
+    def show_image(self, file_name, directory=None):
+        if directory is None:
+            directory = self.controller.file_manager.telechargement_dir
+        with Image.open(os.path.join(directory, file_name)) as img:
             img.show()
+
+    def build_app(self):
+        images = os.listdir(self.controller.file_manager.telechargement_dir)
+        for image in images:
+            self.images[image] = 100
+
+    def compare(self):
+        if len(self.images) < 2:
+            raise ValueError("Il doit y avoir au moins deux images enregistrées. Penser à lancer self.build_app()")
+        image1, image2 = random.sample(list(self.images), 2)
+        self.show_image(image1)
+        self.show_image(image2)
