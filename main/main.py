bd51d2abebf77de86a26e7068bdc379a635c7604
Louis-Saglio <louis.saglio@sfr.fr>
2017-12-18 14:20:08 +0100

    Minimal engine works

--- main/main.py
+++ main/main.py
@@ -1,3 +1,15 @@
 from src.controller import Controller
 
-controller = Controller()
+# todo: log
+
+controller = Controller("Linux")
+controller.theme_manager.add_keyword("Archlinux", 1)
+controller.theme_manager.add_ranked_keywords(2, "Open source", "Manjaro", "Kernel", "Wallpaper")
+controller.theme_manager.add_ranked_keywords(3, "Debian", "Gentoo", "Slackware")
+controller.theme_manager.add_ranked_keywords(4, "Python", "Golang")
+controller.theme_manager.search_options["face"] = False
+for i in range(10):  # TODO: choisir combien d'images télécharger
+    controller.image_downloader.download_images()
+controller.image_comparator.build_app()
+while True:
+    controller.image_comparator.compare()
