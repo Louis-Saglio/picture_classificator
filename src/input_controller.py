da5cc7e437babb87d09f77ecad6fe301d118d569
Louis-Saglio <louis.saglio@sfr.fr>
2017-12-18 20:50:28 +0100

    input() => by using it instead of input() we can perform custom action while receiving special input

--- src/input_controller.py
+++ src/input_controller.py
@@ -3,15 +3,21 @@ class InputController:
     def __init__(self, controller):
         self.controller = controller
 
+    def input(self, message):
+        received = input(message)
+        if received.lower() == 'stop':
+            self.controller.save()
+            exit(0)
+        return received
+
     def get_theme_name(self, auto=True):
         if auto:
             return self.controller.settings.DEFAULT_APP_NAME
-        return input("Choisissez un nom pour ce système de gestion : >>> ")
+        return self.input("Choisissez un nom pour ce système de gestion : >>> ")
 
-    @staticmethod
-    def get_fav_image(nbr=2):
+    def get_fav_image(self, nbr=2):
         while True:
-            rep = input("Choisissez votre image favorite. >>> ")
+            rep = self.input("Choisissez votre image favorite. >>> ")
             try:
                 if 0 < int(rep) <= nbr:
                     return int(rep) - 1
