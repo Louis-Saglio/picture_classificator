2852cb3c4812f87df66900c456f1118e3cc461db
Louis-Saglio <louis.saglio@sfr.fr>
2017-12-18 09:39:33 +0100

    Add get_fav_image()

--- src/input_controller.py
+++ src/input_controller.py
@@ -6,4 +6,15 @@ class InputController:
     def get_theme_name(self, auto=True):
         if auto:
             return self.controller.settings.DEFAULT_APP_NAME
-        return input("Choisissez un nom pour ce système de getsion : >>> ")
+        return input("Choisissez un nom pour ce système de gestion : >>> ")
+
+    @staticmethod
+    def get_fav_image(nbr=2):
+        while True:
+            rep = input("Choisissez votre image favorite. >>> ")
+            try:
+                if 0 < int(rep) <= nbr:
+                    return int(rep) - 1
+            except TypeError:
+                pass
+            print(f"Vous devez choisir un nombre entre 1 et {nbr} inclus")
