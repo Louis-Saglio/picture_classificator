da221299e79aeccfa85cfe017110bcdb867ce346
Louis-Saglio <louis.saglio@sfr.fr>
2017-12-20 14:52:54 +0100

    input() now accept a parameter to auto choose an input value. Mainly used in tests

--- src/input_controller.py
+++ src/input_controller.py
@@ -1,29 +1,40 @@
+import random
+
+import src.exceptions
+
+
 class InputController:
 
     def __init__(self, controller):
+        """
+        :type controller: src.controller.Controller
+        """
         self.controller = controller
 
-    def input(self, message):
-        received = input(message)
+    def input(self, message, possible_values=None):
+        if self.controller.settings.DEBUG:
+            assert possible_values, "En mode debug veuillez fournir une liste des valeurs possibles"
+            received = random.choice(possible_values)
+        else:
+            received = input(message)
         if received.lower() == 'stop':
-            # todo: test
             self.controller.save()
-            exit(0)
+            raise src.exceptions.EndProgramSignal
         return received
 
-    def get_theme_name(self, auto=True):
-        if auto:
-            return self.controller.settings.DEFAULT_APP_NAME
-        # todo: test
-        return self.input("Choisissez un nom pour ce système de gestion : >>> ")
+    def get_theme_name(self):
+        return self.input(
+            "Choisissez un nom pour ce système de gestion : >>> ",
+            (self.controller.settings.DEFAULT_APP_NAME,)
+        )
 
     def get_fav_image(self, nbr=2):
         while True:
-            rep = self.input("Choisissez votre image favorite. >>> ")
+            rep = self.input("Choisissez votre image favorite. >>> ", ('1', '2'))
             try:
                 if 0 < int(rep) <= nbr:
                     return int(rep) - 1
             # todo: test
-            except TypeError:
+            except ValueError:
                 pass
             print(f"Vous devez choisir un nombre entre 1 et {nbr} inclus")
