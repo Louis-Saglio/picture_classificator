2fdfab35cf4b489b4643ca17a5d6d54372341e53
Louis-Saglio <louis.saglio@sfr.fr>
2017-12-21 23:49:30 +0100

    Update to use improved APIs of FileManager and ImageDownloader Set DEBUG setting to False

--- main/main.py
+++ main/main.py
@@ -1,15 +1,21 @@
+import src.exceptions
 from src.controller import Controller
 
 # todo: log
 
 controller = Controller("Linux")
+controller.settings.DEBUG = False
 controller.theme_manager.add_keyword("Archlinux", 1)
 controller.theme_manager.add_ranked_keywords(2, "Open source", "Manjaro", "Kernel", "Wallpaper")
 controller.theme_manager.add_ranked_keywords(3, "Debian", "Gentoo", "Slackware")
 controller.theme_manager.add_ranked_keywords(4, "Python", "Golang")
 controller.theme_manager.search_options["face"] = False
-for i in range(10):  # TODO: choisir combien d'images télécharger
-    controller.image_downloader.download_images()
+controller.image_downloader.download_images(image_nbr=5)
 controller.image_comparator.build_app()
 while True:
-    controller.image_comparator.compare()
+    try:
+        controller.image_comparator.compare()
+    except src.exceptions.EndProgramSignal:
+        print("Le programme s'est terminé normalement.")
+        controller.save()
+        exit(0)
