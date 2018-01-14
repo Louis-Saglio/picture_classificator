3b7ed7acb66c950f5a2019e623f29d5e971ef11f
Louis-Saglio <louis.saglio@sfr.fr>
2018-01-14 20:52:48 +0100

    Refactor exceptions to add a custom base exception Add InvalidSignalError

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,2 +1,10 @@
-class EndProgramSignal(BaseException):
+class PictureClassificatorException(BaseException):
+    pass
+
+
+class EndProgramSignal(PictureClassificatorException):
+    pass
+
+
+class InvalidSignalError(PictureClassificatorException):
     pass
